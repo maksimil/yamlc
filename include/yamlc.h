d4bb414a27f079e31749722acc95eb74933ca8d2
Maxim <maxim.kosterov@gmail.com>
2020-10-29 14:45:14 +0300

    rewrote the header

--- include/yamlc.h
+++ include/yamlc.h
@@ -1,58 +1,71 @@
+#ifndef __H_YAML_C
+#define __H_YAML_C
+
 #include <stdint.h>
 
-// object type
-typedef enum
+typedef enum yc_type
 {
-    // yc_string
+    none,
     string,
-    // yc_list of yc_pairs
-    dictionary,
-    // yc_list of yc_objects
-    list
+    list,
+    object
 } yc_type;
 
-// object, which could be string, dictionary or list
-typedef struct
+typedef struct yc_string
 {
-    void *object;
-    yc_type type;
+    char *data;
+    size_t allocated;
+} yc_string;
+
+typedef struct yc_object
+{
+    yc_string name;
+
+    void *child;
+    yc_type child_type;
+
+    struct yc_object *root;
 } yc_object;
 
-// string
-typedef char *yc_string;
-
-// pair of key and value
-typedef struct
+typedef struct yc_list
 {
-    yc_string key;
-    yc_object value;
-} yc_pair;
-
-// list of values
-typedef struct
-{
-    void **data;
+    yc_object *data;
     size_t size;
     size_t allocated;
 } yc_list;
 
-// list functions
-yc_list *yc_create_list();
-yc_list *yc_create_list(size_t);
-void yc_create_list(yc_list *);
-void yc_create_list(yc_list *, size_t);
-void yc_destroy_list(yc_list *);
+typedef struct yc_emitter
+{
+    yc_object root;
+    yc_object *current;
+} yc_emitter;
 
-// pushes element to list
-void yc_push_el(yc_list *, void *);
-// pushes key-value pair to dictionary
-void yc_push_kw(yc_list *, yc_string, yc_object);
+// string functions
+void yc_initialize_string(yc_string *, char *);
+void yc_set_string(yc_string *, char *);
+void yc_destroy_string(yc_string *);
 
 // object functions
-yc_object *yc_create_object(yc_type);
-void yc_create_object(yc_object *, yc_type);
+void yc_initialize_object(yc_object *, char *name);
 void yc_destroy_object(yc_object *);
 
+// list functions
+void yc_initialize_list(yc_list *, size_t);
+void yc_destroy_list(yc_list *);
+yc_object *yc_push(yc_list *);
+
+// emitter functions
+void yc_initialize_emitter(yc_emitter *);
+void yc_destroy_emitter(yc_emitter *);
+
+void yc_add_property(yc_emitter, char *);
+
+void yc_set_object(yc_emitter *);
+void yc_set_value(yc_emitter *, char *);
+
+void yc_close(yc_emitter *);
+
 // file functions
-void yc_write_file(yc_string, yc_object *);
-void yc_read_file(yc_string, yc_object *);
+void yc_read_file(yc_object *, char *);
+void yc_write_file(yc_object *, char *);
+#endif
